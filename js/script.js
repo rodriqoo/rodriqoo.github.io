e8a7b8241ff8b2d0d1ee259095e50579bde9374d
Rodrigo Bravo <123138189+rodriqoo@users.noreply.github.com>
2023-02-15 15:12:43 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+    if($('body').hasClass('work')){
+        $('.card-slider').slick({
+            arrows: false,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            speed: 1500,
+            infinite: true,
+        })
+    }
+});
+
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
